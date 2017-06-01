44a993aba673b83a6529eb2b543ed23156a58ad7
Jisoo Park <jisoopark95@gmail.com>
2017-06-01 12:18:07 +0900

    fixed dropdown

--- snu_art/db/schema.rb
+++ snu_art/db/schema.rb
@@ -10,6 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170601024551) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "login_id"
+    t.string   "password"
+    t.string   "name"
+    t.text     "description"
+    t.string   "profile_img"
+    t.string   "mail"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
--- snu_art/db/seeds.rb
+++ snu_art/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Artist.create!(login_id: 'jisoo@gmail.com', password: 'pw', name: 'jisoo', description: 'this is a description', profile_img: '1.jpg', mail: 'jisoo@gmail.com')
