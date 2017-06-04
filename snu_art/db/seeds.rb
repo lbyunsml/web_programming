96b34ba872c3e244fcd2d2ccff15c2ae0c460716
Jisoo Park <jisoopark95@gmail.com>
2017-06-04 19:37:44 +0900

    main page displays 4-column layout pics

--- snu_art/db/seeds.rb
+++ snu_art/db/seeds.rb
@@ -9,6 +9,30 @@
 #Artist.create!(login_id: 'jisoo@gmail.com', password: 'pw', name: 'jisoo', description: 'this is a description', profile_img: '1.jpg', mail: 'jisoo@gmail.com')
 #Artist.create!(login_id: 'bob@gmail.com', password: 'pwbob', name: 'bob', description: 'this is bob', profile_img: 'bob.jpg', mail: 'bob@gmail.com')
 
+
+Artwork.create!(name:"a1pink flower", img_path:"work1.png", description:"pink artwork", category:"metal", artist_id:1)
+Artwork.create!(name:"a2pink flower", img_path:"work2.jpg", description:"pink artwork", category:"metal", artist_id:1)
+Artwork.create!(name:"a3pink flower", img_path:"work3.png", description:"pink artwork", category:"metal", artist_id:1)
+Artwork.create!(name:"a4pink flower", img_path:"work4.jpg", description:"pink artwork", category:"metal", artist_id:1)
+Artwork.create!(name:"a5pink flower", img_path:"work5.jpg", description:"pink artwork", category:"metal", artist_id:1)
+Artwork.create!(name:"a5pink flower", img_path:"work6.jpg", description:"pink artwork", category:"metal", artist_id:1)
+Artwork.create!(name:"a5pink flower", img_path:"work7.jpg", description:"pink artwork", category:"metal", artist_id:1)
+Artwork.create!(name:"a5pink flower", img_path:"work8.jpg", description:"pink artwork", category:"metal", artist_id:1)
+Artwork.create!(name:"a5pink flower", img_path:"work9.jpg", description:"pink artwork", category:"metal", artist_id:1)
+Artwork.create!(name:"a5pink flower", img_path:"work10.jpg", description:"pink artwork", category:"metal", artist_id:1)
+Artwork.create!(name:"a5pink flower", img_path:"work11.jpg", description:"pink artwork", category:"metal", artist_id:1)
+Artwork.create!(name:"a5pink flower", img_path:"work12.jpg", description:"pink artwork", category:"metal", artist_id:1)
+Artwork.create!(name:"a5pink flower", img_path:"work13.jpg", description:"pink artwork", category:"metal", artist_id:1)
+Artwork.create!(name:"a5pink flower", img_path:"work14.jpg", description:"pink artwork", category:"metal", artist_id:1)
+Artwork.create!(name:"a5pink flower", img_path:"work15.jpg", description:"pink artwork", category:"metal", artist_id:1)
+Artwork.create!(name:"a5pink flower", img_path:"work16.jpg", description:"pink artwork", category:"metal", artist_id:1)
+Artwork.create!(name:"a5pink flower", img_path:"work17.jpg", description:"pink artwork", category:"metal", artist_id:1)
+Artwork.create!(name:"a5pink flower", img_path:"work18.jpg", description:"pink artwork", category:"metal", artist_id:1)
+Artwork.create!(name:"a5pink flower", img_path:"work19.jpg", description:"pink artwork", category:"metal", artist_id:1)
+Artwork.create!(name:"a5pink flower", img_path:"work20.jpg", description:"pink artwork", category:"metal", artist_id:1)
+
+
+
 #Artwork.create!(name:"artwork1", img_path:"img1.jpg", description:"work1", category:"metal", artist_id:1)
 #Artwork.create!(name:"artwork2", img_path:"img2.jpg", description:"work2", category:"metal", artist_id:1)
 #Artwork.create!(name:"artwork3", img_path:"img3.jpg", description:"work3", category:"metal", artist_id:1)
